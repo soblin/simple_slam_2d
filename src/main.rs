0b5f4312e85c196513aff981fea23bc297751435
soblin <hilo.soblin@gmail.com>
2022-08-24 09:23:52 +0900

    second commit
    Signed-off-by: soblin <hilo.soblin@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::fs;
+use std::fs::File;
+use std::io::{self, prelude::*, BufReader};
 use std::path::Path;
 use std::process::Command;
 
@@ -12,8 +13,16 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .output()?;
 
     let pkg_share_directory = String::from_utf8(cmd.stdout)?;
-    println!("pkg_share_directory = {}", pkg_share_directory);
+    let pkg_share_directory = pkg_share_directory.trim();
 
     // (2) then open data/*.lsc
+    let pkg_share_directory = Path::new(&pkg_share_directory);
+    let data_directory = pkg_share_directory.join("dataset");
+    let file_name = String::from("corridor.lsc");
+    let lsc_file_path = data_directory.join(&file_name);
+    let fp = BufReader::new(File::open(lsc_file_path).expect("Unable to open file"));
+    println!("there are {} lines.", fp.lines().count());
+
+    // (3) main loop: iteratively read each line and process them
     Ok(())
 }
