434c15c6d6077f459b86ae24f95e5d733e0a1110
soblin <hilo.soblin@gmail.com>
2022-09-11 10:53:38 +0900

    working with std_msgs/Header
    Signed-off-by: soblin <hilo.soblin@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ fn main() -> Result<(), Error> {
         "/burger1_scan",
         rclrs::QOS_PROFILE_SENSOR_DATA,
         move |msg: sensor_msgs::msg::LaserScan| {
-            println!("LaserScan info:\nangle_min = {}\nangle_max = {}\nscan_time = {}\nrange_min = {}\nrange_max = {}\nnumber of point is {}", msg.angle_min, msg.angle_max, msg.scan_time, msg.range_min, msg.range_max, msg.ranges.len());
+            println!("scan points: #{}", msg.ranges.len());
         },
     )?;
 
@@ -21,5 +21,23 @@ fn main() -> Result<(), Error> {
             println!("Odometry is x = {}, y = {}", position.x, position.y);
         },
     )?;
+
+    let pub_map = node.create_publisher::<nav_msgs::msg::OccupancyGrid>(
+        "/burger1/map",
+        rclrs::QOS_PROFILE_SENSOR_DATA,
+    )?;
+
+    while context.ok() {
+        let mut occ_grid_msg = nav_msgs::msg::OccupancyGrid::default();
+        occ_grid_msg.header.frame_id = String::from("burger1");
+        let cur_time = std::time::SystemTime::now();
+        let cur_time = cur_time.duration_since(std::time::SystemTime::UNIX_EPOCH)?;
+        occ_grid_msg.header.stamp = builtin_interfaces::msg::Time {
+            sec: cur_time.as_secs() as i32,
+            nanosec: cur_time.subsec_nanos() as u32,
+        };
+        println!("{:?}", occ_grid_msg.header.stamp);
+        std::thread::sleep(std::time::Duration::from_millis(500));
+    }
     rclrs::spin(&node).map_err(|err| err.into())
 }
