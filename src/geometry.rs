7c61185ee99189ce4fdf741657addc9661240bcb
soblin <hilo.soblin@gmail.com>
2022-10-02 17:26:21 +0900

    changed to f64
    Signed-off-by: soblin <hilo.soblin@gmail.com>

--- src/geometry.rs
+++ src/geometry.rs
@@ -1,20 +1,20 @@
 use crate::utils;
 
-pub fn quat2rpy(quat: &[f32; 4]) -> [f32; 3] {
+pub fn quat2rpy(quat: &[f64; 4]) -> [f64; 3] {
     let (q0, q1, q2, q3) = (quat[0], quat[1], quat[2], quat[3]);
-    let r: f32 = (2.0 * (q0 * q1 + q2 * q3)).atan2(q0 * q0 - q1 * q1 - q2 * q2 + q3 * q3);
-    let p: f32 = (2.0 * (q0 * q2 - q1 * q3)).asin();
-    let y: f32 = (2.0 * (q0 * q3 + q1 * q2)).atan2(q0 * q0 + q1 * q1 - q2 * q2 - q3 * q3);
+    let r: f64 = (2.0 * (q0 * q1 + q2 * q3)).atan2(q0 * q0 - q1 * q1 - q2 * q2 + q3 * q3);
+    let p: f64 = (2.0 * (q0 * q2 - q1 * q3)).asin();
+    let y: f64 = (2.0 * (q0 * q3 + q1 * q2)).atan2(q0 * q0 + q1 * q1 - q2 * q2 - q3 * q3);
     [r, p, y]
 }
 
-pub fn rpy2quat(rpy: &[f32; 3]) -> [f32; 4] {
-    let cos_r: f32 = (rpy[0] / 2.0).cos();
-    let sin_r: f32 = (rpy[0] / 2.0).sin();
-    let cos_p: f32 = (rpy[1] / 2.0).cos();
-    let sin_p: f32 = (rpy[1] / 2.0).sin();
-    let cos_y: f32 = (rpy[2] / 2.0).cos();
-    let sin_y: f32 = (rpy[2] / 2.0).sin();
+pub fn rpy2quat(rpy: &[f64; 3]) -> [f64; 4] {
+    let cos_r: f64 = (rpy[0] / 2.0).cos();
+    let sin_r: f64 = (rpy[0] / 2.0).sin();
+    let cos_p: f64 = (rpy[1] / 2.0).cos();
+    let sin_p: f64 = (rpy[1] / 2.0).sin();
+    let cos_y: f64 = (rpy[2] / 2.0).cos();
+    let sin_y: f64 = (rpy[2] / 2.0).sin();
     return [
         sin_r * cos_p * cos_y - cos_r * sin_p * sin_y, // x
         cos_r * sin_p * cos_y + sin_r * cos_p * sin_y, // y
@@ -25,9 +25,9 @@ pub fn rpy2quat(rpy: &[f32; 3]) -> [f32; 4] {
 
 #[derive(Default, Clone)]
 pub struct Pose2D {
-    pub x: f32,
-    pub y: f32,
-    pub th: f32,
+    pub x: f64,
+    pub y: f64,
+    pub th: f64,
 }
 
 impl Pose2D {
@@ -47,7 +47,7 @@ impl Pose2D {
             },
         };
     }
-    pub fn integrate(&mut self, v: f32, omega: f32, dt: f32) {
+    pub fn integrate(&mut self, v: f64, omega: f64, dt: f64) {
         let (dx, dy, dth) = (v * self.th.cos(), v * self.th.sin(), omega);
         self.x += dx * dt;
         self.y += dy * dt;
@@ -55,7 +55,7 @@ impl Pose2D {
     }
 }
 
-pub fn polar2cartesian(base: &Pose2D, radius: f32, angle: f32) -> Pose2D {
+pub fn polar2cartesian(base: &Pose2D, radius: f64, angle: f64) -> Pose2D {
     let th = base.th + angle;
     return Pose2D {
         x: base.x + radius * th.cos(),
@@ -66,14 +66,14 @@ pub fn polar2cartesian(base: &Pose2D, radius: f32, angle: f32) -> Pose2D {
 
 pub struct OdomIntegrator {
     pub odom: Pose2D,
-    pub v: f32,
-    pub omega: f32,
+    pub v: f64,
+    pub omega: f64,
     pub stamp: Option<builtin_interfaces::msg::Time>,
     pub stopped: bool,
 }
 
 impl OdomIntegrator {
-    pub fn new(x: f32, y: f32, th: f32) -> Self {
+    pub fn new(x: f64, y: f64, th: f64) -> Self {
         OdomIntegrator {
             odom: Pose2D { x: x, y: y, th: th },
             v: 0.0,
@@ -83,8 +83,8 @@ impl OdomIntegrator {
         }
     }
     pub fn update_velocity(&mut self, v: f32, omega: f32) {
-        self.v = v;
-        self.omega = omega;
+        self.v = v as f64;
+        self.omega = omega as f64;
         if v.abs() < 1e-5 && omega.abs() < 1e-5 {
             self.stopped = true;
         } else {
@@ -92,15 +92,13 @@ impl OdomIntegrator {
         }
     }
     pub fn update_pose(&mut self, pose: &Pose2D) {
-        if let Some(stamp) = &self.stamp {
-            self.odom = pose.clone();
-        }
+        self.odom = pose.clone();
         self.stamp = Some(utils::get_stamp());
     }
     pub fn integrate_pose(&mut self) {
         if let Some(stamp) = &self.stamp {
             if !self.stopped {
-                let dt = utils::elapsed_ms(&stamp) as f32 / 1000.0;
+                let dt = utils::elapsed_ms(&stamp) as f64 / 1000.0;
                 self.odom.integrate(self.v, self.omega, dt);
             }
         }
--- src/slam.rs
+++ src/slam.rs
@@ -2,7 +2,7 @@ use crate::geometry;
 
 // TODO: trait for slam method
 pub struct OdometryMapping {
-    scan: Vec<(f32, f32)>, // (radius, angle)
+    scan: Vec<(f64, f64)>, // (radius, angle)
     pub points: Vec<geometry_msgs::msg::Point32>,
     pub channels: Vec<f32>,
 }
@@ -17,14 +17,14 @@ impl OdometryMapping {
     }
     pub fn set_scan(&mut self, scan: sensor_msgs::msg::LaserScan) {
         // these are in radian
-        let angle_min = scan.angle_min;
-        let angle_increment = scan.angle_increment;
+        let angle_min = scan.angle_min as f64;
+        let angle_increment = scan.angle_increment as f64;
         for (i, range) in scan.ranges.iter().enumerate() {
             if *range == std::f32::INFINITY {
                 continue;
             }
-            let angle = angle_min + (i as f32) * angle_increment;
-            self.scan.push((*range, angle));
+            let angle = angle_min + (i as f64) * angle_increment;
+            self.scan.push((*range as f64, angle));
         }
     }
     pub fn do_slam(&mut self, pose: &geometry::Pose2D) {
@@ -36,8 +36,8 @@ impl OdometryMapping {
         for (radius, angle) in self.scan.iter() {
             let p = geometry::polar2cartesian(pose, *radius, *angle);
             self.points.push(geometry_msgs::msg::Point32 {
-                x: p.x,
-                y: p.y,
+                x: p.x as f32,
+                y: p.y as f32,
                 z: 0.0,
             });
             self.channels.push(color_float);
@@ -46,7 +46,7 @@ impl OdometryMapping {
 }
 
 pub struct ICPMapping {
-    scan: Vec<(f32, f32)>, // (radius, angle)
+    scan: Vec<(f64, f64)>, // (radius, angle)
     ref_map: Vec<geometry_msgs::msg::Point32>,
     pub poses: Vec<geometry::Pose2D>, // poses.last() is the latest estimation
     pub points: Vec<geometry_msgs::msg::Point32>,
@@ -65,14 +65,14 @@ impl ICPMapping {
     }
     pub fn set_scan(&mut self, scan: sensor_msgs::msg::LaserScan) {
         // these are in radian
-        let angle_min = scan.angle_min;
-        let angle_increment = scan.angle_increment;
+        let angle_min = scan.angle_min as f64;
+        let angle_increment = scan.angle_increment as f64;
         for (i, range) in scan.ranges.iter().enumerate() {
             if *range == std::f32::INFINITY {
                 continue;
             }
-            let angle = angle_min + (i as f32) * angle_increment;
-            self.scan.push((*range, angle));
+            let angle = angle_min + (i as f64) * angle_increment;
+            self.scan.push((*range as f64, angle));
         }
     }
     pub fn do_slam(&mut self, pose: &geometry::Pose2D) -> geometry::Pose2D {
@@ -85,8 +85,8 @@ impl ICPMapping {
             for (radius, angle) in self.scan.iter() {
                 let p = geometry::polar2cartesian(pose, *radius, *angle);
                 let p = geometry_msgs::msg::Point32 {
-                    x: p.x,
-                    y: p.y,
+                    x: p.x as f32,
+                    y: p.y as f32,
                     z: 0.0,
                 };
                 self.ref_map.push(p.clone());
@@ -102,11 +102,9 @@ impl ICPMapping {
         let mut scan_pairs = vec![];
         for (radius, angle) in self.scan.iter() {
             let pose = geometry::polar2cartesian(last_pose, *radius, *angle);
-            let (mut dmax, mut ind) = (std::f32::INFINITY, 0);
+            let (mut dmax, mut ind) = (std::f64::INFINITY, 0);
             for (j, point) in self.ref_map.iter().enumerate() {
-                let dist = ((pose.x - point.x).powi(2) + (pose.y - point.y).powi(2))
-                    .sqrt()
-                    .abs();
+                let dist = (pose.x - point.x as f64).powi(2) + (pose.y - point.y as f64).powi(2);
                 if dist < dmax {
                     dmax = dist;
                     ind = j;
@@ -115,20 +113,21 @@ impl ICPMapping {
             scan_pairs.push(ind)
         }
         // iteratively update last_pos to new pose
-        let ll = 0.01;
+        let ll = 0.0001;
         let f_thre = 0.01;
-        let (dd, dth) = (0.1, 0.01);
+        let (dd, dth) = (0.1, 0.1);
         let max_iter = 50;
         let mut est_pose = last_pose.clone();
         let mut opt_pose = last_pose.clone();
         let mut fprev = self.icp_score(&scan_pairs, est_pose.x, est_pose.y, est_pose.th);
-        let mut fmin = std::f32::INFINITY;
+        let mut fmin = std::f64::INFINITY;
         for i in 1..=max_iter {
             let f = fprev;
             let fx = self.icp_score(&scan_pairs, est_pose.x + dd, est_pose.y, est_pose.th);
             let fy = self.icp_score(&scan_pairs, est_pose.x, est_pose.y + dd, est_pose.th);
             let fth = self.icp_score(&scan_pairs, est_pose.x, est_pose.y, est_pose.th + dth);
             let (dx, dy, dth) = ((fx - f) / dd, (fy - f) / dd, (fth - f) / dth);
+            println!("dx = {}, dy = {}, dth = {}", dx, dy, dth);
             est_pose.x -= dx * ll;
             est_pose.y -= dy * ll;
             est_pose.th -= dth * ll;
@@ -146,22 +145,24 @@ impl ICPMapping {
         for (range, angle) in self.scan.iter() {
             let p = geometry::polar2cartesian(&opt_pose, *range, *angle);
             self.points.push(geometry_msgs::msg::Point32 {
-                x: p.x,
-                y: p.y,
+                x: p.x as f32,
+                y: p.y as f32,
                 z: 0.0,
             });
             self.channels.push(color_float);
         }
         return opt_pose;
     }
-    fn icp_score(&self, scan_pairs: &Vec<usize>, x: f32, y: f32, th: f32) -> f32 {
+    fn icp_score(&self, scan_pairs: &Vec<usize>, x: f64, y: f64, th: f64) -> f64 {
         let mut score = 0.0;
         for i in 0..scan_pairs.len() {
             let j = scan_pairs[i];
-            let (radius, angle): &(f32, f32) = &self.scan[i];
-            let pt_x = x + radius * angle.cos();
-            let pt_y = y + radius * angle.sin();
-            let dist = (pt_x - (self.ref_map[j].x)).powi(2) + (pt_y - self.ref_map[j].y).powi(2);
+            let (radius, angle): &(f64, f64) = &self.scan[i];
+            let th = angle + th;
+            let pt_x = x + radius * th.cos();
+            let pt_y = y + radius * th.sin();
+            let dist = (pt_x - self.ref_map[j].x as f64).powi(2)
+                + (pt_y - self.ref_map[j].y as f64).powi(2);
             score += dist;
         }
         return score;
